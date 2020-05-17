44cdc9a952af4833e0a4c7b264eac717ba481ca7
Landon F <larobis4@gmail.com>
2020-05-17 02:29:48 -0500

    Fully functional game area. No loss or win.

--- MineSweeper/MineSweeper.js
+++ MineSweeper/MineSweeper.js
@@ -27,7 +27,7 @@ class gameSpaceObject {
     }
 
     isBomb() {
-        if (this.state == "hiddenBomb") {
+        if (this.state == "hiddenBomb" || this.state == "shownBomb") {
             return true;
         }
         return false;
@@ -40,6 +40,11 @@ class gameSpaceObject {
     showImage() {
         if (this.hiddenImage) {
             this.object.style.backgroundImage = `url(${this.hiddenImage})`;
+            if (this.state == "hiddenBomb" || this.state == "shownBomb") {
+                this.state = "shownBomb";
+            } else {
+                this.state = "shown";
+            }
         }
     }
 }
@@ -49,36 +54,87 @@ class gameBoard {
         this.board = [];
         this.width = width;
         this.height = height;
+        this.boardID = BoardElementID;
+        this.bombs = bombs;
+        this.firstClick = false;
         this.handleClick = this.handleClick.bind(this);
-        for (let i = 0; i < height; i++ ) {
+        this.generateBoard();
+    }
+
+    generateBoard() {
+        //generates a board of the appropriate length
+        if (this.board.length > 0) {
+            this.deleteBoard();
+        }
+        for (let i = 0; i < this.height; i++ ) {
             let testRow = document.createElement("div");
-            document.getElementById(BoardElementID).appendChild(testRow);
+            document.getElementById(this.boardID).appendChild(testRow);
             testRow.id = `tableRow${i}`;
             this.board[i] = [];
-            for (let j = 0; j < width; j++ ) {
-                this.board[i][j] = new gameSpaceObject(`${500 / width}px`, `${500 / width}px`, `tableRow${i}`, `${i}.${j}`, this.handleClick);
+            for (let j = 0; j < this.width; j++ ) {
+                this.board[i][j] = new gameSpaceObject(`${500 / this.width}px`, `${500 / this.width}px`, `tableRow${i}`, `${i}.${j}`, this.handleClick);
             }
         }
-        this.generateBombs(bombs);
-        console.log(this.board);
+        this.generateBombs();
+    }
+
+    deleteBoard() {
+        //deletes all the elements from the DOM
+        for (let i = this.board.length - 1; i >= 0; i--) {
+            for (let j = this.board[0].length - 1; j >= 0; j--) {
+                this.board[i][j].deleteObject();
+            }
+        }
     }
 
     handleClick(event) {
-        // console.log(event);
         let id = event.target.id;
         let middleIndex = id.indexOf(".");
-        console.log(id.substring(0, middleIndex) + " " + id.substring(middleIndex + 1));
-        if (this.board[id.substring(0, middleIndex)][id.substring(middleIndex + 1)].isBomb()) {
-            this.board[id.substring(0, middleIndex)][id.substring(middleIndex + 1)].showImage();
-        } else {
-            this.board[id.substring(0, middleIndex)][id.substring(middleIndex + 1)].setBackground(numUrls[this.getObjectNum(parseInt(id.substring(0, middleIndex)), parseInt(id.substring(middleIndex + 1)))]);
-            // console.log(this.board[id.substring(0, middleIndex)][id.substring(middleIndex + 1)].hiddenimage);
-            this.board[id.substring(0, middleIndex)][id.substring(middleIndex + 1)].showImage();
-        }
+        this.uncoverElement(parseInt(id.substring(0, middleIndex)), parseInt(id.substring(middleIndex + 1)));
+    }
 
+    uncoverElement(x, y) {
+        if (this.board[x][y].isBomb()) {
+            if (!this.firstClick) {
+                //makes sure the first click is an open space
+                resetGame();
+                game.handleClick(event);
+                return;
+            }
+            this.board[x][y].showImage();
+        } else {
+            //this finds out what number image to assign the block
+            let num = this.getObjectNum(x, y);
+            if (!this.firstClick && num != 0) {
+                //makes sure the first click is an open space
+                resetGame();
+                game.handleClick(event);
+                return;
+            }
+            this.firstClick = true;
+            this.board[x][y].setBackground(numUrls[num]);
+            this.board[x][y].showImage();
+            if (num == 0) {
+                for (let i = -1; i < 2; i++) {
+                    for (let j = -1; j < 2; j++) {
+                        if (this.board[x + i] && !(i == 0 && j == 0)) {
+                            if (this.board[x + i][y+j]) {
+                                if (this.board[x+i][y+j].state != "shown") {
+                                    // console.log(i + " " + j);
+                                    this.uncoverElement(x + i, y + j);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+        }
     }
 
     getObjectNum(i, j) {
+        //figures out how many bombs are around an object
         let count = 0;
         for (let k = -1; k < 2; k++) {
             for (let l = -1; l < 2; l++) {
@@ -94,9 +150,10 @@ class gameBoard {
         return count;
     }
 
-    generateBombs(bombs) {
+    generateBombs() {
+        //puts the bombs on the generated grid
         let i = 0;
-        while (i < bombs) {
+        while (i < this.bombs) {
             let attempt = this.board[Math.floor(Math.random() * this.board.length)][Math.floor(Math.random() * this.board[0].length)];
             if (attempt.state != "hiddenBomb") {
                 attempt.makeBomb();
@@ -107,15 +164,18 @@ class gameBoard {
 
     deleteBoard() {
         for (let i = this.height - 1; i >= 0; i--) {
-            for (let j = this.width; j >= 0; j--) {
+            for (let j = this.width - 1; j >= 0; j--) {
                 this.board[j][i].deleteObject();
             }
         }
     }
 }
 let game = new gameBoard(15, 15, "game-area", 56);
-function changeURL() {
-    game.board[0][0].object.style.backgroundImage = `url(${numUrls[Math.floor(Math.random() * numUrls.length)]})`;
+function resetGame() {
+    if (game.board.length > 0) {
+        game.deleteBoard();
+    }
+    game = new gameBoard(15, 15, "game-area", 56);
 }
 // document.getElementById("startButton").addEventListener("onclick", changeURL);
 
